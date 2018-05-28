7391a6c49d607a0945b301aa8580e264579c70ad
Alistair Kane <alistairkane92@hotmail.co.uk>
2018-05-28 10:01:51 +0100

    adds a basic skeleton

---
+++ src/main/java/Alan.java
@@ -0,0 +1,15 @@
+public class Alan {
+    private boolean doesItWork;
+
+    public Alan() {
+        this.doesItWork = false;
+    }
+
+    public boolean isDoesItWork() {
+        return doesItWork;
+    }
+
+    public void makeItWork() {
+        this.doesItWork = true;
+    }
+}
